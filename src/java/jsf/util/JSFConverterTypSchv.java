ef84d65897af8a20541b401946ff69735708f1d5
sosyn <isosyn@mukrnov.cz>
2017-09-23 21:49:13 +0200

    Uprava implemetace tridy Converter pro jednotlive ciselnikove typy

---
+++ src/java/jsf/util/JSFConverterTypSchv.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf.util;
+
+import entity.Typschv;
+import java.util.UUID;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Ivo
+ */
+@Named("JSFConverterTypSchv")
+@Stateless
+public class JSFConverterTypSchv implements Converter {
+
+    @EJB
+    private ejb.TypSchvFacade ejbTypSchvFacade;
+
+    @Override
+    public Object getAsObject(FacesContext fc, UIComponent uic, String string) {
+        UUID uuid = UUID.fromString(string);
+        Typschv typSchv = this.ejbTypSchvFacade.find(uuid);
+        return typSchv;
+    }
+
+    @Override
+    public String getAsString(FacesContext fc, UIComponent uic, Object o) {
+        if (o != null) {
+            return ((Typschv) o).getId().toString();
+        } else {
+            return null;
+        }
+    }
+
+}
